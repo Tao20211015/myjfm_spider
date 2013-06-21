6d576f7d2e2160fccd0d9acfac849e52898d6636
myjfm <myjfm@localhost.localdomain>
2013-06-22 01:38:44 +0800

    modify the run_ut.sh script to run the hash tests

--- src/run_ut.sh
+++ src/run_ut.sh
@@ -3,10 +3,20 @@
 ./utility_ut
 if [ $? -eq 0 ]; then
   echo
-  echo 'SUCCESS! All Unit Tests passed!'
+  echo 'SUCCESS! All utility unit tests passed!'
 else
   echo
-  echo 'FAILED! Some Unit Test Cases are failed!'
+  echo 'FAILED! Some utility unit test cases are failed!'
+  exit 1
+fi
+
+./hash_test
+if [ $? -eq 0 ]; then
+  echo
+  echo 'SUCCESS! All hash function tests passed!'
+else
+  echo
+  echo 'FAILED! Some hash function test Cases are failed!'
   exit 1
 fi
 
